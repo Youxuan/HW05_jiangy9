f4a3aae086065d51fe5ee074ac23bb1532e18b08
Youxuan(Lucy) Jiang <jiangy9@muohio.edu>
2012-11-26 19:55:14 -0800

    commit_2
    Fix most mistakes, but does not work. Runtime error

--- HW05_jiangy9/ListGraph.cpp
+++ HW05_jiangy9/ListGraph.cpp
@@ -1,65 +1,70 @@
 #include "ListGraph.h"
 #include <list>
 
-ListGraph::ListGraph(){
-	num_edges = 0;
-}
-
 ListGraph::ListGraph(int numNodes){
-	EList(numNodes, NWPair(0,0));
+	EList* list = new EList();
+	for(int j=0;j<numNodes;j++)
+		(*list).push_back(NWPair(0,0));
 	for(int i=0;i<numNodes;i++){
-		edgeList[i].assign(EList.begin(), EList.end());
+		edgeList[i].assign((*list).begin(), (*list).end());
 	}
 	num_edges = 0;
 }
 
 void ListGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){
-	if(0 <= u < edgeList.size() && 0 <= v < edgeList.size() && u!=v && weight > 0){
-		NWPair pair = edgeList[u].back();
-		for(int i=0;i<edgeList.size();i++){
-			if(pair.first == v && pair.second != 0)
-				pair.second = weight;
-			if(pair != edgeList[u].front())
-				std::advance(pair,i+1);
-		}
+	NWPair pair = edgeList[u].front();
+	for(int i=0;i<edgeList.size();i++){
+		if(pair.first == v && pair.second == 0)
+			pair.second = weight;
+		if(pair.first != edgeList[u].back().first)
+			std::advance(pair,i+1);
 	}
+
+	pair = edgeList[v].front();
+	for(int i=0;i<edgeList.size();i++){
+		if(pair.first == u && pair.second == 0)
+			pair.second = weight;
+		if(pair.first != edgeList[v].back().first)
+			std::advance(pair,i+1);
+	}
+	
 }
 
 EdgeWeight ListGraph::weight(NodeID u, NodeID v) const{
 	double result = 0.0;
-	if(0 <= u < edgeList.size() && 0 <= v < edgeList.size()){
-		NWPair pair = edgeList[u].back();
-		for(int i=0;i<edgeList.size();i++){
-			if(pair.first == v && pair.second != 0)
-				result = pair.second;
-			if(pair != edgeList[u].front())
-				std::advance(pair,i+1);
-		}
+	NWPair pair = edgeList[u].front();
+	for(int i=0;i<edgeList.size();i++){
+		if(pair.first == v)
+			result = pair.second;
+		else
+			std::advance(pair,i+1);
 	}
+	
 	return result;
 }
 
 std::list<NWPair> ListGraph::getAdj(NodeID u) const{
+	EList* list = new EList();
 	if(0 <= u < edgeList.size()){
-		NWPair pair = edgeList[u].back();
+		NWPair pair = edgeList[u].front();
 		for(int i=0;i<edgeList.size();i++){
 			if(pair.second != 0)
-				EList.push_back(pair);;
-			if(pair != edgeList[u].front())
+				(*list).push_back(pair);;
+			if(pair.first != edgeList[u].back().first)
 				std::advance(pair,i+1);
 		}
 	}
-	return EList;
+	return *list;
 }
 
 unsigned ListGraph::degree(NodeID u) const{
 	int count = 0;
 	if(0 <= u < edgeList.size()){
-		NWPair pair = edgeList[u].back();
+		NWPair pair = edgeList[u].front();
 		for(int i=0;i<edgeList.size();i++){
 			if(pair.second != 0)
 					count += 1;
-			if(pair != edgeList[u].front())
+			if(pair.first != edgeList[u].back().first)
 				std::advance(pair,i+1);
 		}
 	}
@@ -67,7 +72,7 @@ unsigned ListGraph::degree(NodeID u) const{
 }
 
 unsigned ListGraph::size() const{
-	return edgeList.size()*edgeList.size();
+	return edgeList.size();
 }
 
 unsigned ListGraph::numEdges() const{
--- HW05_jiangy9/MatrixGraph.cpp
+++ HW05_jiangy9/MatrixGraph.cpp
@@ -1,55 +1,51 @@
 #include "MatrixGraph.h"
 #include <list>
 
-MatrixGraph::MatrixGraph(){
-	M[0][0] = 0;
-	num_edges = 0;
-}
-
 MatrixGraph::MatrixGraph(unsigned num_nodes){
 	for(int i=0; i < num_nodes; i++){
-		for(int j=0; j < num_nodes; j++)
+		for(int j=0; j < num_nodes; j++){
 			M[i][j] = 0;
+			M[j][i] = 0;
+		}
 	}
 	num_edges = 0;
 }
 
 void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){
-	if(0 <= u < M.size() && 0 <= v < M.size() && u!=v && M[u][v]==0 && weight > 0){
 		M[u][v] = weight;
+		M[v][u] = weight;
 		num_edges++;
-	}
 }
 
 EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const{
-	if(0 <= u < M.size() && 0 <= v < M.size())
 		return M[u][v];
 }
 
 std::list<NWPair> MatrixGraph::getAdj(NodeID u) const{
-	if(0 <= u < M.size()){
-		for(int i = 0; i < M[u].size(); i++){
-			if(M[u][i]!=0){
-				EList.push_back(NWPair(i, M[u][i]));
-			}
+	EList* list = new EList();
+	NWPair pair;
+	for(int i = 0; i < M[u].size(); i++){
+		if(M[u][i]!=0){
+			pair.first = i;
+			pair.second = M[u][i];
+			(*list).push_back(pair);
 		}
 	}
-	return EList;
+	return *list;
 }
 
 unsigned MatrixGraph::degree(NodeID u) const{
 	int count = 0;
-	if(0 <= u < M.size()){
-		for(int i=0; i < M[u].size(); i++){
-			if(M[u][i]!=0)
-				count += 1;
-		}
+	for(int i=0; i < M[u].size(); i++){
+		if(M[u][i]!=0)
+			count += 1;
 	}
+	
 	return count;
 }
 
 unsigned MatrixGraph::size() const{
-	return M.size()*M.size();
+	return M.size();
 }
 
 unsigned MatrixGraph::numEdges() const{
